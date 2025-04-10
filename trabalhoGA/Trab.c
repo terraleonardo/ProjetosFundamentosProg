ecb8f2838869fd3db144f226f2893f44a9e53687
Leonardo Terra dos Santos <LeonardoSantos17@edu.unisinos.br>
2025-04-09 21:07:44 -0300

    refazendo começo do trabalhoGA

--- trabalhoGA/Trab.c
+++ trabalhoGA/Trab.c
@@ -1,47 +1,48 @@
 #include <stdio.h>
-
-void printaMenuInicial() {
-    printf("Digite a opção desejada:\n1 - Nova Simulação\n2 - Sair do Programa\n");
-}
+    
+    // void printaMenu(char opcoes[]) {
+    //     printf("Digite a opção desejada:");
+    //     for(int i = 0; i < sizeof(opcoes) / sizeof(opcoes[0]); i++) {
+    //         printf("%d - %s\n", i+1, opcoes[i]);
+    //     }
+    // }
 
 int main() {
 
-    int opcao;
-    int populacaoTotal;
-    int infectadosIniciais;
-    double taxaContagio;
-    double porcentagemVacinados;
-    double efetividadeVac;
-    int novosInfectados;
+    char opcoesMenuInicial[2][32] = {"Nova Simulação", "Sair do programa"};
+    int indexMenuInicial;
 
-    char dados[6][64] = {"População total", "Nº de pessoas inicialmente infectadas", "Taxa de contágio", "Porcentagem da população infectada", "Efetividade da vacina", "Nº de dias da simulação"};
-    int tamArray = sizeof(dados) / sizeof(dados[0]);
+    do { //Printa o menu inicial equanto o usuário não digitar uma opção váida
+        printf("Digite a opção desejada:\n");
 
-    printaMenuInicial();
-    scanf("%d", &opcao);
+        for(int i = 0; i < sizeof(opcoesMenuInicial) / sizeof(opcoesMenuInicial[0]); i++) {
+            printf("%d - %s\n", i+1, opcoesMenuInicial[i]);
+        }
 
-    if(opcao == 1) {
-        int dadosInt[2];
-        int dadosDouble[3];
-        
-        for(int i = 0; i < tamArray; i++) {
-            printf("%s\n", dados[i]);
-            if(i == 0 || i == 1 || i == 5) { //CONFERIR LOGICA AQUI
-                scanf("%d", &dadosInt[i]);
-            } else {
-                scanf("%lf", &dadosDouble[i]);
+        scanf("%d", &indexMenuInicial);
+
+        if(indexMenuInicial == 1) {
+            char opcoesMenuSimulacao[6][64] = {"População total", "Nº de pessoas inicialmente infectadas", "Taxa de contágio", "Porcentagem da população infectada", "Efetividade da vacina", "Nº de dias da simulação"};
+            float dados[6];
+
+            printf("\nDigite os dados requisitados a seguir:\n");
+            for(int i = 0; i < sizeof(opcoesMenuSimulacao) / sizeof(opcoesMenuSimulacao[0]); i++) {
+
+                printf("%s: ", opcoesMenuSimulacao[i]);
+                scanf("%f", &dados[i]);
             }
-        }
 
-        for (int i = 0; i < sizeof(dadosInt) / sizeof(dadosInt[0]); i++) {
-            printf("%d\n", dadosInt[i]);
+            printf("\nNumeros na variável dados:\n");
+            for(int j = 0; j < 6; j++){
+                printf("%f\n", dados[j]);
+            }
 
+        } else if(indexMenuInicial == 2) {
+            printf("Até logo!");
+        } else {
+            printf("Opção inváida!\n");
         }
-        for (int i = 0; i < sizeof(dadosInt) / sizeof(dadosInt[0]); i++) {
-            printf("%lf\n", dadosDouble[i]);
-        }
-
-    }
+    } while(indexMenuInicial != 2 && indexMenuInicial != 1);
 
     return 0;
 }
