a1bb3c2ae9046466e6b62239634b18e9d03220d9
Leonardo Terra dos Santos <LeonardoSantos17@edu.unisinos.br>
2025-04-23 21:59:55 -0300

    fazendo trabalho na aula

--- trabalhoGA/Trab.c
+++ trabalhoGA/Trab.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 
-// void printaMenu(char opcoes[]) {
-//     printf("Digite a opção desejada:");
-//     for(int i = 0; i < sizeof(opcoes) / sizeof(opcoes[0]); i++) {
-//         printf("%d - %s\n", i+1, opcoes[i]);
-//     }
-// }
+int min(int a, int b) {
+    return (a < b) ? a : b;
+}
 
-// tentando fazer função que retorne tamanho do array
-/* int tamanho(int array[]) {
-    return sizeof(array) / sizeof(array[0]);
-} */
+int sortearNumero() {
+    srand(time(NULL));
+
+    return (rand() % 100) + 1;
+}
 
 int main() {
     char opcoesMenuInicial[2][32] = {"Nova Simulação", "Sair do programa"};
@@ -56,21 +55,83 @@ int main() {
                 }
             }
 
-            printf("\nDados na variável dadosInt:\n");
-            for(int j = 0; j < 3; j++) {
-                printf("[%d] %d\n", j, dadosInt[j]);
+            int populacaoTotal = dadosInt[0];
+            int inicialmenteInfectados = dadosInt[1];
+            float taxaContagio = dadosFloat[0];
+            float porcentagemVacinada = dadosFloat[1];
+            float efetividadeVacina = dadosFloat[2];
+            int diasSimulacao = dadosInt[2];
+
+            // printf("\nDados na variável dadosInt:\n"); //TESTES
+            // for(int j = 0; j < 3; j++) {
+            //     printf("[%d] %d\n", j, dadosInt[j]);
+            // }
+
+            // printf("\nDados na variável dadosFloat:\n");
+            // for(int h = 0; h < 3; h++) {
+            //     printf("[%d] %.2f\n", h, dadosFloat[h]);
+            // }
+
+            int infectados = dadosInt[1];
+
+            printf("Dia 1: %d infectados", infectados);
+
+            for(int i = 1; i <= diasSimulacao; i++) {
+                if(i>1) {
+
+                    int numero = sortearNumero(); // AJUSTAR PARA SORTEAR NUMERO DIFERENTE A CADA INSTANCIA DO LOOP
+                    int evento;
+
+                    char *nomeEvento = malloc(32);
+
+                    if(numero > 0 && numero <= 15) {
+                        evento = 25;
+                        strcpy(nomeEvento, "Aglomeracao");
+                    } else if(numero > 15 && numero <= 25) {
+                        evento = -20;
+                        strcpy(nomeEvento, "Isolamento voluntario");
+                    } else if(numero > 25 && numero <= 30) {
+                        evento = -30;
+                        strcpy(nomeEvento, "Midia gera alerta");
+                    } else if(numero > 30 && numero <= 35) {
+                        evento = 50;
+                        strcpy(nomeEvento, "Mutacao mais contagiosa");
+                    } else {
+                        evento = 0;
+                    }
+
+
+                    int novos_infectados = abs(inicialmenteInfectados * ((taxaContagio+evento)/100));
+
+                    int nao_vacinados_saudaveis = (populacaoTotal - inicialmenteInfectados) - (1-(porcentagemVacinada/100));
+
+                    int vacinados_saudaveis = (populacaoTotal - inicialmenteInfectados)*(porcentagemVacinada/100);
+
+                    int suscetiveis = nao_vacinados_saudaveis + vacinados_saudaveis * (1-(efetividadeVacina/100));
+
+                    int novosInfectados = min(novos_infectados, suscetiveis);
+
+                    infectados += novosInfectados;
+
+                    if(evento == 0) {
+                        printf("\nDia %d: %d infectados (+%d)", i, infectados, novosInfectados); //Falta eventos aleatórios e nº de infectados por dia
+                    } else {
+                        printf("\nDia %d: %d infectados (+%d) (evento: %s)", i, infectados, novosInfectados, nomeEvento); //Falta eventos aleatórios e nº de infectados por dia
+                    }
+
+                    // IF PARA VER SE O Nº SUSCETIVEIS JA ATINGIRAM O Nº DE INFECTADOS
+                }
             }
 
-            printf("\nDados na variável dadosFloat:\n");
-            for(int h = 0; h < 3; h++) {
-                printf("[%d] %.2f\n", h, dadosFloat[h]);
-            }
+
 
         } else if(indexMenuInicial == 2) {
             printf("Até logo!");
         } else {
             printf("Opção inválida!\n");
         }
+
+
     } while(indexMenuInicial != 2 && indexMenuInicial != 1);
 
     return 0;
