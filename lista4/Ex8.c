94e68d15069ffbd163d89e85bedb792fb4cd08ff
terraleonardo <terraleonardo21@gmail.com>
2025-05-21 19:41:37 -0300

    terminando ex 8

--- lista4/Ex8.c
+++ lista4/Ex8.c
@@ -3,22 +3,34 @@
 int main() {
 
     int num;
-    int resultado;
+    int resultado = 1;
+    char opcao = 'n';
 
-    printf("Entre com um número: ");
-    scanf("%d", &num);
+    do {
+        printf("\nEntre com um número: ");
+        scanf("%d", &num);
 
-    int numeros[num];
+        int numeros[num];
 
-    for(int i = 0; i < num; i++) {
-        numeros[i] = i+1;
-    }
+        for(int i = 0; i < num; i++) {
+            numeros[i] = i+1;
+        }
 
-    for(int i = 1; i < num; i++) {
-        resultado *= numeros[i];
-    }
+        for(int i = 1; i < num; i++) {
+            resultado *= numeros[i];
+            // numeros[i] = 0;
+        }
 
-    printf("\nResultado: %d", resultado);
+        printf("\nO fatorial de %d é %d", num, resultado);
+        do {
+            printf("\nCalcular outro resultado (s/n)? ");
+            scanf(" %c", &opcao);
+            if(opcao != 'n' && opcao != 'N' && opcao != 's' && opcao != 'S') {
+                printf("\nDigite uma entrada válida! ");
+            }
+        } while(opcao != 'n' && opcao != 'N' && opcao != 's' && opcao != 'S');
+        resultado = 1;
+    } while(opcao != 'n' && opcao != 'N');
 
     return 0;
 }
