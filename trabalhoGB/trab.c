9dc0fcdde2e702356c7d51779fadd00b888b5d93
Leonardo Santos <terraleonardo21@gmail.com>
2025-06-02 16:48:53 -0300

    mudando nomenclaturas e implementando mais detalhes

--- trabalhoGB/trab.c
+++ trabalhoGB/trab.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <time.h>
+
+int randomInt(int min, int max) { // Definindo função random
+    return min + rand() % (max - min + 1);
+}
 
 typedef struct { // Definindo struct Personagem
-    char classe[32];
+    char classe[16];
     int vida;
     int ataque;
     int defesa;
@@ -21,24 +27,31 @@ Personagem personagem(char classe[16], int ataque, int defesa) { // Definindo co
     return p;
 }
 
-void inicializaTime(Personagem time[]) { // Definindo inicializador de time
-    time[0] = personagem("Guerreiro", 20, 10);
-    time[1] = personagem("Mago", 30, 5);
-    time[2] = personagem("Cacador", 18, 8);
-    time[3] = personagem("Paladino", 15, 12);
-    time[4] = personagem("Barbaro", 25, 6);
+void inicializaEquipe(Personagem equipe[]) { // Definindo inicializador de equipe
+    equipe[0] = personagem("Guerreiro", 20, 10);
+    equipe[1] = personagem("Mago", 30, 5);
+    equipe[2] = personagem("Cacador", 18, 8);
+    equipe[3] = personagem("Paladino", 15, 12);
+    equipe[4] = personagem("Barbaro", 25, 6);
+}
+
+int sorteiaPrimeiro() {
+    return randomInt(1, 2);
 }
 
 int main() {
+    srand(time(NULL)); // Inicializando seed da função random
+    
+    #define TAMANHO_EQUIPE 5
 
-    Personagem time1[5];
-    Personagem time2[5];
+    Personagem equipe1[TAMANHO_EQUIPE];
+    Personagem equipe2[TAMANHO_EQUIPE];
 
-    inicializaTime(time1);
-    inicializaTime(time2);
+    inicializaEquipe(equipe1);
+    inicializaEquipe(equipe2);
 
-    // for(int i=0; i<5; i++) { // TESTANDO SE TODAS AS CLASSES FORAM CRIADAS
-    //     printf("\n%s\nHP:%d ATK:%d DEF:%d HAB_BOOL:%d\n", time1[i].classe, time1[i].vida, time1[i].ataque, time1[i].defesa, time1[i].habilidade_ativa);
+    // for(int i=0; i<TAMANHO_EQUIPE; i++) { // TESTANDO SE TODAS AS CLASSES FORAM CRIADAS
+    //     printf("\n%s\nHP:%d ATK:%d DEF:%d HAB_BOOL:%d\n", equipe1[i].classe, equipe1[i].vida, equipe1[i].ataque, equipe1[i].defesa, equipe1[i].habilidade_ativa);
     // }
     // printf("\n");
 
